17acf39dc32237edac185504231cdce5e7108c83
Himal Patel <hnp.2792@gmail.com>
2026-03-27 11:17:29 +0000

    latest working

--- models/User.js
+++ models/User.js
@@ -6,7 +6,9 @@ const User = sequelize.define('User', {
     firebase_uid: { type: DataTypes.STRING, unique: true, allowNull: false },
     fcm_token: { type: DataTypes.STRING, allowNull: true },
     purchase_id: { type: DataTypes.STRING, unique: true, allowNull: true },
-    fcm_token_change_count: { type: DataTypes.INTEGER, defaultValue: 0 }
+    trial_start_date: { type: DataTypes.DATEONLY, allowNull: true },
+    fcm_token_change_count: { type: DataTypes.INTEGER, defaultValue: 0 },
+    notification_count: { type: DataTypes.INTEGER, defaultValue: 0 }
 }, {
     tableName: 'tbl_users',
     timestamps: true,
